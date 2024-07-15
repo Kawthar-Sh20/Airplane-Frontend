ef9bd3d70d6aeff10b9f124a809939dd851b5ec7
hady-awayda <hady.awayda@gmail.com>
2024-07-16 01:06:52 +0300

    Added register form

--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -1,23 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   const registerForm = document.getElementById("register-form");
   registerForm.addEventListener("click", (e) => {
-    console.log("Register page");
-    // e.preventDefault();
-    // const formData = new FormData(registerForm);
-    // console.log(formData);
-    // fetch("http://localhost/api/register", {
-    //   method: "POST",
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //   },
-    //   body: JSON.stringify({
-    //     email: formData.get("email"),
-    //     password: formData.get("password"),
-    //   }),
-    // })
-    //   .then((response) => response.json())
-    //   .then((data) => {
-    //     console.log(data);
-    //   });
+    e.preventDefault();
+    const formData = new FormData(registerForm);
+    console.log(formData.get("name"));
+    console.log(formData.get("email"));
+    console.log(formData.get("password"));
+    fetch("http://localhost/api/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: formData.get("name"),
+        email: formData.get("email"),
+        password: formData.get("password"),
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      });
   });
 });
